d768ba2bf781bf49b275dff6576a57d3e0a88219
Yash Thanki <yash@unoapp.com>
2018-09-19 20:18:49 -0400

    ex-4

--- Lab3/exercise-4/finished.js
+++ Lab3/exercise-4/finished.js
@@ -2,9 +2,22 @@ var max = 50
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-var callBackWait = function (counter, callback) {
-    let randomWaitTime = getRandomInt(0,5000)
-    setInterval()
-}
+
 var handleCounter = function (err, counter, waitTime, timeStamp) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('Counter: ' + counter, 'wait: '+ waitTime, 'Time Stamp: ' + timeStamp)
+    }
+}
+var callBackWait = function (counter, callback) {
+        let randomWaitTime = getRandomInt(0,5000)
+        setTimeout(function () {
+        callback(null, counter, randomWaitTime/1000+ 's', + new Date())
+        } ,randomWaitTime)
+}
+
+
+for(let i = 1; i <= 10; i++) {
+    callBackWait(i, handleCounter);
 }
